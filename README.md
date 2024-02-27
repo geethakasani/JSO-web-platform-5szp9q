38e1391f479a9adfde00332828777bd4f82303dc
geethakasani <160598661+geethakasani@users.noreply.github.com>
2024-02-27 16:40:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JSO-web-platform-5szp9q
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/geethakasani/JSO-web-platform-5szp9q)
---
+++ script.js
@@ -0,0 +1,96 @@
+var x = 5;
+var y = 2;
+
+// Challenge 1: Perform the addition operation (add x and y) and print the answer in the console.
+Add = x + y;
+console.log(Add);
+
+// Challenge 2: Perform the Subtraction operation (subtract y from x) and print the answer in the console.
+sub = y - x;
+console.log(y - x);
+// Challenge 3: Perform Multiplication operation (multiply x from y) and print the answer in console.
+M = x * y;
+console.log(M);
+// Challenge 4: Perform Division operation (Divide x by y) and print the answer in console.
+D = x / y;
+console.log(D);
+// Challenge 5: perform division operation (divide x by y) and print the remainder in console.
+d = x % y;
+console.log(d);
+// Challenge 6: Perform Exponent operation and print the answer in console.
+e = Math.pow(5, 2);
+console.log(e);
+var z = 20;
+// Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
+console.log(e);
+// Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+n = --z;
+console.log(n);
+
+var a = 20;
+
+var b = 10;
+
+var c = 15;
+
+var d = 20;
+
+// Challenge 9: Perform Addition assignment operation to the variable a;
+a += 3;
+console.log(a);
+// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+b -= 2;
+console.log(b);
+// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+c *= 3;
+console.log(c);
+// Challenge 12: Perform 	Division assignment operation to the variable c;
+c /= 3;
+console.log(c);
+// Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+var num1 = 6;
+var num2 = 6;
+var Equal = num1 == num2;
+console.log(Equal);
+var str1 = 'Hi';
+var str2 = 'Welcome';
+var notEqual = str1 == str2;
+console.log(notEqual);
+// Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+var string1 = 'hello';
+var string2 = 'hello';
+var isNotE = string1 != string2;
+console.log(isNotE);
+var N = 5;
+var T = 10;
+var iSEqual = N != T;
+console.log(iSEqual);
+
+// Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+var FNum = 10;
+var SNun = 15;
+var tNum = 11;
+var F = SNun < FNum;
+console.log(F);
+S = tNum < FNum;
+console.log(S);
+// Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+var Num1 = 12;
+var Num2 = 1;
+var Num3 = 20;
+var A = Num1 > Num2;
+var B = Num2 > Num3;
+console.log(A);
+console.log(B);
+// Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+
+var Num4 = 20;
+var E = Num1 <= Num2;
+console.log(E);
+var NE = Num3 <= Num4;
+console.log(NE);
+// Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+var GE = Num1 >= Num2;
+var GN = Num2 >= Num1;
+console.log(GE);
+console.log(GN);
